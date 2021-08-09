5c9dfe64a22f140d40d2b5befeabf6d1a3832cd0
johnaduke <73294147+johnaduke@users.noreply.github.com>
2021-08-09 11:08:23 -0400

    changed subnet output name

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
-output "public_subnets" {
+output "subnet_id" {
   value = ["${aws_subnet.subnet_public.id}"]
 }
 output "public_route_table_ids" {
